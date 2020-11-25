1e7b363422e04d743b5532045ee7cb7a80172b33
SereneCai <73454440+SereneCai@users.noreply.github.com>
2020-11-25 14:14:06 +0800

    modified javascript for testergame

--- testergame/testergamescript.js
+++ testergame/testergamescript.js
@@ -6,21 +6,36 @@ function jump()
     if (character.classList != "animate")
     {
         character.classList.add("animate"); 
+
     }
 
     setTimeout (function(){
         character.classList.remove("animate"); 
     } , 500)
+       
 }
+
+var score = 1;
+
+function scoreCount()
+{
+    document.getElementById("score").innerHTML = score++;
+}
+
 
 var endGame = setInterval(function(){
     var charTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var obLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("margin-left"));
+    
     if (obLeft < 20 && obLeft >0 && charTop >= 130)
     {
+        
         obstacle.style.display = "none";
         alert ("You Lose!");
+        document.getElementById("score").innerHTML = "The score is resetted.";
+        score = 0;
     }
+
+   
 },10)
 
-//
